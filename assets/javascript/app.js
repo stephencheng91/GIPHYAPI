1cfdabd29d8b9e5e1e40722f06c2356ffda44107
KuanToto <stephencheng91@gmail.com>
2019-04-04 18:44:24 -0700

    rev -

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,38 +6,38 @@
 var apiKey = "8L4aVQa394LnTX4I6B98AtiQCSyY7hHo";
 //input take in as id
 var input = ""
+var image;
 
-
-$("#submit").click(function(event){
+$("#submit").click(function (event) {
     event.preventDefault();
 
 
-    input=$("#inputText").val();
+    input = $("#inputText").val();
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    input + "&api_key=" +apiKey;
+        input + "&api_key=" + apiKey;
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         console.log(response);
         //getting url from server ;
         var gifData = response.data
         //create image div in html
-        for(var i=0; i<gifData.length;i++){
+        for (var i = 0; i < gifData.length; i++) {
 
-            var imageUrl= gifData[i].images.original.url;
+            var imageUrl = gifData[i].images.original.url;
 
-            var image = $("<img>");
+            image = $("<img>");
 
             image.attr("src", imageUrl);
             image.attr("alt", input + " image");
-    
+
             $(".results").prepend(image);
         }
-        
+
     })
-    
+
     createButtons();
     //Checking input and url works
     console.log(queryURL);
@@ -45,10 +45,43 @@ $("#submit").click(function(event){
 
 })
 
-function createButtons(){
+function createButtons() {
     var button = $("<button>");
-    
+
     button.text(input);
 
     $(".searchedButton").prepend(button);
+
+    $(".searchedButton").click(function (event) {
+        event.preventDefault();
+        $(".results").empty();
+
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            $(".searchedButton").text() + "&api_key=" + apiKey;
+
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            //getting url from server ;
+            var gifData = response.data
+            //create image div in html
+            for (var i = 0; i < gifData.length; i++) {
+
+                var imageUrl = gifData[i].images.original.url;
+
+                image = $("<img>");
+
+                image.attr("src", imageUrl);
+                image.attr("alt", input + " image");
+
+                $(".results").prepend(image);
+            }
+        })
+    })
 }
+
+
