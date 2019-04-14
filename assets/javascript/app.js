e020ddf30de72d32fbfb1a25bd33596beefb0bb5
KuanToto <stephencheng91@gmail.com>
2019-04-14 11:29:23 -0700

    adding pause and run function for gif

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,11 @@ var apiKey = "8L4aVQa394LnTX4I6B98AtiQCSyY7hHo";
 //input take in as id
 var input = "";
 var image;
+var topics = [];
 
 $("#submit").click(function (event) {
     event.preventDefault();
-
-
+    $(".results").empty();
     input = $("#inputText").val();
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         input + "&api_key=" + apiKey;
@@ -27,17 +27,28 @@ $("#submit").click(function (event) {
         for (var i = 0; i < gifData.length; i++) {
 
             var imageUrl = gifData[i].images.original.url;
+            var imageUrlAlt = gifData[i].images.original_still.url;
 
             image = $("<img>");
 
+            image.addClass("clickImage");
             image.attr("src", imageUrl);
+            image.attr("alt-src", imageUrlAlt);
             image.attr("alt", input + " image");
 
             $(".results").prepend(image);
         }
+        $(".clickImage").on("click", function (event) {
+            event.preventDefault();
+            var imageCurrent = $(this).attr("src");
+            var imageAlt = $(this).attr("alt-src");
+            $(this).attr("src", imageAlt);
+            $(this).attr("alt-src", imageCurrent);
+        })
 
     })
-
+    topics.push(input);
+    console.log(topics)
     createButtons();
     //Checking input and url works
     console.log(queryURL);
@@ -48,19 +59,20 @@ $("#submit").click(function (event) {
 function createButtons() {
 
     // create a button with id=input
-
+    $(".searchedButton").empty();
     //  newButton =  button.text(input);
-    var newButton = $("<button>");
-    newButton.addClass("newButton");
-    newButton.attr("id", input);
-    newButton.text(input);
-
-
-
-    $(".searchedButton").prepend(newButton);
+    for (var i = 0; i < topics.length; i++) {
+        var newButton = $("<button>");
+        newButton.addClass("newButton");
+        newButton.attr("id", topics[i]);
+        newButton.text(topics[i]);
+        console.log("createButtons")
+        $(".searchedButton").prepend(newButton);
+    }
 
     $(".newButton").click(function (event) {
         event.preventDefault();
+        console.log("onclick")
         $(".results").empty();
 
         var topic = $(this).attr("id");
@@ -80,14 +92,25 @@ function createButtons() {
             for (var i = 0; i < gifData.length; i++) {
 
                 var imageUrl = gifData[i].images.original.url;
+                var imageUrlAlt = gifData[i].images.original_still.url;
 
                 image = $("<img>");
 
+                image.addClass("clickImage");
                 image.attr("src", imageUrl);
+                image.attr("alt-src", imageUrlAlt);
                 image.attr("alt", input + " image");
 
                 $(".results").prepend(image);
             }
+            $(".clickImage").on("click", function (event) {
+                event.preventDefault();
+                var imageCurrent = $(this).attr("src");
+                var imageAlt = $(this).attr("alt-src");
+                $(this).attr("src", imageAlt);
+                $(this).attr("alt-src", imageCurrent);
+            })
+
         })
     })
 }
